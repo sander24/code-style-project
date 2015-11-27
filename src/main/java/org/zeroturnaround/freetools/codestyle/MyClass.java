ec012c28fcd230353467743682472a7dca79ea3a
Sander Sonajalg <sander.sonajalg@gmail.com>
2015-11-27 18:47:13 +0200

    changes

--- src/main/java/org/zeroturnaround/freetools/codestyle/MyClass.java
+++ src/main/java/org/zeroturnaround/freetools/codestyle/MyClass.java
@@ -2,12 +2,23 @@ package org.zeroturnaround.freetools.codestyle;
 
 public class MyClass {
   
+  
   public static void main(String[] args) {
     System.out.println("something something");
     System
     .out 
+    
         .println(    "what  an  aaawful style")
         ;
   }
+  
+  
    
+  
+    public String getMoreCowbell() {
+ // oh my god the indentation is reaaaaaly fucked up 
+           return 
+               "MOAAARRR COWBELL!!!";
+           
+    } 
 }
