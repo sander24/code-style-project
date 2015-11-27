bfd5736129c48d58567b2735e3238ac2870a5ac5
Sander Sonajalg <sander.sonajalg@gmail.com>
2015-11-27 18:13:11 +0200

    initial

---
+++ src/main/java/org/zeroturnaround/freetools/codestyle/MyClass.java
@@ -0,0 +1,13 @@
+package org.zeroturnaround.freetools.codestyle;
+
+public class MyClass {
+  
+  public static void main(String[] args) {
+    System.out.println("something something");
+    System
+    .out 
+        .println(    "what  an  aaawful style")
+        ;
+  }
+   
+}
